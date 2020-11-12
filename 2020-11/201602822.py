b305428bb1dd8c89b57d6f17672b7179aeb52f9b
FherHerand <fer.h.j@live.com>
2020-11-12 15:26:39 -0600

    data countries graph

--- 2020-11/201602822.py
+++ 2020-11/201602822.py
@@ -1,5 +1,7 @@
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -23,9 +25,29 @@ print(URL_CONFIRMED)
 data_confirmed = requests.get(URL_CONFIRMED).content
 df = pd.read_csv(io.StringIO(data_confirmed.decode('utf-8')))
 
-df_ca = df[df['Country/Region'].isin(['Guatemala', 'Honduras', 'El Salvador', 'Costa Rica', 'Nicaragua', 'Panama', 'Belice'])]
-data_ca = df_ca.values
-print(data_ca)
+df_ca_countries = df[df['Country/Region'].isin(['Guatemala', 'Honduras', 'El Salvador', 'Costa Rica', 'Nicaragua', 'Panama', 'Belize'])]
+data_ca_countries = df_ca_countries.values
+print(data_ca_countries)
 
-dates = df_ca.columns.values[4:]
-formatted_dates = list(map(lambda date_str: datetime(2000+int(date_str.split('/')[2]), int(date_str.split('/')[0]), int(date_str.split('/')[1])).strftime('%d/%m/%Y'), dates))# strptime(date_str, '%-m/%-d/%-y').strftime('%d/%m/%Y'), dates))
+dates = df_ca_countries.columns.values[4:]
+formatted_dates = dates
+#formatted_dates = list(map(lambda date_str: datetime(2000+int(date_str.split('/')[2]), int(date_str.split('/')[0]), int(date_str.split('/')[1])).strftime('%d/%m/%Y'), dates))# strptime(date_str, '%-m/%-d/%-y').strftime('%d/%m/%Y'), dates))
+
+degree=9
+for i in range(len(data_ca_countries)):
+    X = np.array(range(len(dates)))
+    x_max = X.max()
+    X_seq = np.linspace(X.min(), x_max, x_max+1).reshape(-1, 1)
+    Y = data_ca_countries[i][4:]
+    
+    polyreg = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+    polyreg.fit(X[:, np.newaxis], Y)
+    Y_predicted = polyreg.predict(X_seq)
+    
+    plt.scatter(range(len(dates)), data_ca_countries[i][4:], color='gray')
+    plt.plot(X_seq, Y_predicted, color='coral', linewidth=4)
+    plt.grid()
+    plt.xlabel('Days from 01/22/2020')
+    plt.ylabel('Confirmed cases')
+    plt.title('%s' % data_ca_countries[i][1])
+    plt.show()
