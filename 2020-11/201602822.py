fbf35e69b5423e626a8145f5c028ec1a0ee04974
FherHerand <fer.h.j@live.com>
2020-11-13 22:14:35 -0600

    population data ca countries

--- 2020-11/201602822.py
+++ 2020-11/201602822.py
@@ -20,28 +20,38 @@ data_path = os.path.join(
 )
 
 URL_CONFIRMED = os.path.join(data_path, 'time_series_covid19_confirmed_global.csv')
-print(URL_CONFIRMED)
-
+print('Confirmed Cases Source URL:', URL_CONFIRMED)
 data_confirmed = requests.get(URL_CONFIRMED).content
-df = pd.read_csv(io.StringIO(data_confirmed.decode('utf-8')))
+df_cases_contries = pd.read_csv(io.StringIO(data_confirmed.decode('utf-8')))
 
-df_ca_countries = df[df['Country/Region'].isin(['Guatemala', 'Honduras', 'El Salvador', 'Costa Rica', 'Nicaragua', 'Panama', 'Belize'])]
-data_ca_countries = df_ca_countries.values
-print(data_ca_countries)
+#Data based on worldometers.info and the latest United Nations Population Division estimates
+ca_country_names = ['Guatemala', 'Belize', 'Honduras', 'El Salvador', 'Nicaragua', 'Costa Rica', 'Panama']
+popu_ca_countries = {
+    'Country': ca_country_names,
+    'Population': [17915568, 397628, 9904607, 6486205, 6624554, 5094118, 4314767], #Absolute
+    'Population_m': [17916, 398, 9905, 6486, 6625, 5094, 4315], #P/1000
+    'Density': [167, 17, 89, 313, 55, 65, 58], #P/Km
+}
+df_popu_ca_countries = pd.DataFrame(popu_ca_countries, columns=['Country', 'Population', 'Density'])
 
-dates = df_ca_countries.columns.values[4:]
+ca_population_total = df_popu_ca_countries['Population'].sum()
+print('CA Population:', ca_population_total)
+
+df_cases_ca_countries = df_cases_contries[df_cases_contries['Country/Region'].isin(ca_country_names)]
+data_cases_ca_countries = df_cases_ca_countries.values
+
+dates = df_cases_ca_countries.columns.values[4:]
 formatted_dates = dates
 #formatted_dates = list(map(lambda date_str: datetime(2000+int(date_str.split('/')[2]), int(date_str.split('/')[0]), int(date_str.split('/')[1])).strftime('%d/%m/%Y'), dates))# strptime(date_str, '%-m/%-d/%-y').strftime('%d/%m/%Y'), dates))
 
-degree=9
+degree = 9
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink']
 plots = []
-country_names = []
-for i in range(len(data_ca_countries)):
+for i in range(len(data_cases_ca_countries)):
     X = np.array(range(len(dates)))
     x_max = X.max()
     X_seq = np.linspace(X.min(), x_max, x_max+1).reshape(-1, 1)
-    Y = data_ca_countries[i][4:]
+    Y = data_cases_ca_countries[i][4:]
     
     polyreg = make_pipeline(PolynomialFeatures(degree), LinearRegression())
     polyreg.fit(X[:, np.newaxis], Y)
@@ -50,9 +60,9 @@ for i in range(len(data_ca_countries)):
     #plt.scatter(range(len(dates)), data_ca_countries[i][4:], color='gray')
     plot, = plt.plot(X_seq, Y_predicted, color=colors[i], linewidth=3)
     plots.append(plot)
-    country_names.append(data_ca_countries[i][1])
+    
 plt.grid()
-plt.legend(plots, country_names)#, ncol=3, fontsize=8)
+plt.legend(plots, ca_country_names)
 plt.xlabel('Days from 01/22/2020')
 plt.ylabel('Confirmed cases')
 plt.title('Confirmed cases by Central America countries')
