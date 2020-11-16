06004c83e9e90059dd5c546f2979a184792a4ab8
CiberVeliz <Ciberveliz97@gmail.com>
2020-11-15 19:19:33 -0600

    adding format & comments

--- 2020-11/src/201602959.py
+++ 2020-11/src/201602959.py
@@ -7,14 +7,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 
+#api_url = https://covidtracking.com/data/api <- api covid of US
+
 response = requests.get("https://api.covidtracking.com/v1/us/daily.json")
 data_source = response.json()
 
 input_data = []
 
-for d in data_source:
-    testIncrease = d['totalTestResultsIncrease'] or 0
-    positiveIncrease = d['positiveIncrease'] or 0
+for item in data_source:
+    #new tests
+    testIncrease = item['totalTestResultsIncrease'] or 0
+
+    #new cases
+    positiveIncrease = item['positiveIncrease'] or 0
     
     input_data.append([testIncrease, positiveIncrease])
 
@@ -23,6 +28,7 @@ X = np.array(input_data)
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(X)
 
+print(' --- Centroids --- ')
 print(kmeans.cluster_centers_)
 
 plt.scatter(X[:,0],X[:,1], c=kmeans.labels_, cmap='rainbow')
